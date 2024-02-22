bdbf54c56b9330e2252a26d1c9f895c9f8274f70
unknown <arbaazuniquegzp@gmail.com>
2024-02-22 20:00:01 +0530

    cahnge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000 || process.env.PORT;
+const port=process.env.PORT || 8000
 
 // Define a route that redirects to another link
 app.get('/', (req, res) => {
